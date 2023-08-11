d9c0258eeb96dae1043933968b3776ec786a091c
nikonew <spiritofadv@yandex.ru>
2023-08-11 22:22:12 +0300

    Выполнил задания 1.18. Время разбивать камни

--- src/view/list-event-view.js
+++ src/view/list-event-view.js
@@ -1,7 +1,7 @@
 import { createElement } from '../render';
 
 function createEventEditTemplate() {
-  return  (`
+  return (`
     <li class="trip-events__item">
       <form class="event event--edit" action="#" method="post">
         <header class="event__header">
