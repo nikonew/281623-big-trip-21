350395b280b55f16ea6ed24070a0c9c0dffcc950
nikonew <spiritofadv@yandex.ru>
2023-09-12 23:10:26 +0300

    4.16. Разделяй и властвуй 1. Добавил классам приватные поля и геттеры

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -6,24 +6,27 @@ import {render} from '../framework/render.js';
 import {POINTS_COUNT} from '../const.js';
 
 export default class BoardPresenter {
-  listComponent = new EditList();
-  listEventEdit = new EventEditView();
-  listSort = new SortView();
+  #listComponent = new EditList();
+  #listEventEdit = new EventEditView();
+  #listSort = new SortView();
+  #pointsModel = null;
+  #boardContainer = null;
+  #listPoints = [];
 
   constructor({boardContainer, pointsModel}) {
-    this.boardContainer = boardContainer;
-    this.pointsModel = pointsModel;
+    this.#boardContainer = boardContainer;
+    this.#pointsModel = pointsModel;
   }
 
   init() {
-    this.listPoints = [...this.pointsModel.getPoints()];
+    this.#listPoints = [...this.#pointsModel.points];
 
-    render(this.listSort, this.boardContainer);
-    render(this.listComponent, this.boardContainer);
-    render(this.listEventEdit, this.listComponent.element);
+    render(this.#listSort, this.#boardContainer);
+    render(this.#listComponent, this.#boardContainer);
+    render(this.#listEventEdit, this.#listComponent.element);
 
     for (let i = 0; i < POINTS_COUNT; i++) {
-      render(new PointView({point: this.listPoints[i]}), this.listComponent.element);
+      render(new PointView({point: this.#listPoints[i]}), this.#listComponent.element);
     }
   }
 }
