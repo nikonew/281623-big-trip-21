4fcbf2c9fdfa832300dede1a7d5c73e7ff9b1205
nikonew <spiritofadv@yandex.ru>
2023-09-18 00:12:19 +0300

    4.16. Разделяй и властвуй показ формы, отображение элементов

--- src/view/list-event-view.js
+++ src/view/list-event-view.js
@@ -100,7 +100,7 @@ function createEventEditTemplate({type, basePrice, destination, offers}) {
           </section>
           <section class="event__section  event__section--destination">
             <h3 class="event__section-title  event__section-title--destination">Destination</h3>
-            <p class="event__destination-description">Geneva is a city in Switzerland that lies at the southern tip of expansive Lac Léman (Lake Geneva). Surrounded by the Alps and Jura mountains, the city has views of dramatic Mont Blanc.</p>
+            <p class="event__destination-description">${destination.description}</p>
             <div class="event__photos-container">
               <div class="event__photos-tape">
                 <img class="event__photo" src="img/photos/1.jpg" alt="Event photo">
