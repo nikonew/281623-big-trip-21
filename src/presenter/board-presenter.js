abdb0aa10d299fca6c755dd3a8fc281c44919f72
nikonew <spiritofadv@yandex.ru>
2023-09-26 22:38:27 +0300

    5.8. Большие перемены (часть 1) Выделит в презентере рендеринг компонентов

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -3,12 +3,13 @@ import EditList from '../view/list-view.js';
 import SortView from '../view/list-sort.js';
 import PointView from '../view/list-point.js';
 import EventEditView from '../view/list-event-view.js';
-import {render,replace} from '../framework/render.js';
+import {render,replace, RenderPosition} from '../framework/render.js';
 import NoPointView from '../view/no-task-view.js';
 
 export default class BoardPresenter {
   #listComponent = new EditList();
   #listSort = new SortView();
+  #noPointComponent = new NoPointView();
   #pointsModel = null;
   #boardContainer = null;
   #listPoints = [];
@@ -18,13 +19,7 @@ export default class BoardPresenter {
     this.#pointsModel = pointsModel;
   }
 
-  init() {
-    render(this.#listSort, this.#boardContainer);
-    this.#listPoints = [...this.#pointsModel.points];
-    this.#renderPointList();
-  }
-
-  #renderTask(point) {
+  #renderPoint(point) {
     const escKeyDownHandler = (evt) => {
       if (isEscapeKey(evt)) {
         evt.preventDefault();
@@ -61,15 +56,34 @@ export default class BoardPresenter {
     render(pointComponent, this.#listComponent.element);
   }
 
-  #renderPointList() {
+  #renderSort() {
+    render(this.#listSort, this.#boardContainer, RenderPosition.AFTERBEGIN);
+  }
+
+  #renderNoPoint() {
+    render(this.#noPointComponent, this.#boardContainer);
+  }
+
+  #renderPointList () {
+    this.#listPoints.map((item) => {
+      this.#renderPoint(item);
+    });
+  }
+
+  #renderPointComponent() {
     if(this.#listPoints.length === 0) {
-      render(new NoPointView(), this.#boardContainer);
+      render(this.#renderNoPoint());
       return;
     }
 
     render(this.#listComponent, this.#boardContainer);
-    this.#listPoints.map((item) => {
-      this.#renderTask(item);
-    });
+    this.#renderSort();
+    this.#renderPointList();
+
+  }
+
+  init() {
+    this.#listPoints = [...this.#pointsModel.points];
+    this.#renderPointComponent();
   }
 }
