2e537263152d3cd5cfb902ed9201b3ea94154ec5
nikonew <spiritofadv@yandex.ru>
2023-09-29 22:36:30 +0300

    5.8. Большие перемены (часть 2) Поправит шаблон сортировки

--- src/view/list-sort.js
+++ src/view/list-sort.js
@@ -1,10 +1,12 @@
+import {SORT_TYPE} from '../const.js';
 import AbstractView from '../framework/view/abstract-view.js';
 
 function createSortTemplate() {
   return (
     `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
             <div class="trip-sort__item  trip-sort__item--day">
-              <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day">
+              <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day" data-sort-type="${SORT_TYPE.DAY}" checked>
+
               <label class="trip-sort__btn" for="sort-day">Day</label>
             </div>
 
@@ -14,12 +16,12 @@ function createSortTemplate() {
             </div>
 
             <div class="trip-sort__item  trip-sort__item--time">
-              <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
+              <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time" data-sort-type="${SORT_TYPE.TIME}">
               <label class="trip-sort__btn" for="sort-time">Time</label>
             </div>
 
             <div class="trip-sort__item  trip-sort__item--price">
-              <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price" checked>
+              <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price" data-sort-type="${SORT_TYPE.PRICE}">
               <label class="trip-sort__btn" for="sort-price">Price</label>
             </div>
 
