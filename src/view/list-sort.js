afb00ae87432b24f29074b9c14ad9bd3d21929c7
nikonew <spiritofadv@yandex.ru>
2023-10-03 22:40:41 +0300

    5.8. Большие перемены (часть 2) добавил заглавную букву в выпадающий список

--- src/view/list-sort.js
+++ src/view/list-sort.js
@@ -3,18 +3,8 @@ import AbstractView from '../framework/view/abstract-view.js';
 function getSortItem({type, isChecked, isDisabled}) {
   return `
     <div class="trip-sort__item  trip-sort__item--${type}">
-      <input
-        id="sort-${type}"
-        class="trip-sort__input visually-hidden"
-        type="radio"
-        name="trip-sort"
-        value="sort-${type}"
-        ${(isChecked) ? 'сhecked' : null}
-        ${(isDisabled) ? 'disabled' : null}
-        data-sort-type="${type}">
-      <label
-        class="trip-sort__btn"
-        for="sort-${type}">${type}</label>
+      <input id="sort-${type}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${type}" ${isChecked ? 'checked' : ''} ${isDisabled ? 'disabled' : ''} data-sort-type="${type}">
+      <label class="trip-sort__btn" for="sort-${type}">${type}</label>
     </div>
   `;
 }
